5ca498e334debb8ebce94ca6c3ccfc700f91ba5c
Vaibhav Vishal <vvishal@usc.edu>
2020-10-23 10:48:45 -0700

    Flask Shell comments infile

--- microblog.py
+++ microblog.py
@@ -1,6 +1,6 @@
-from app import app, db
-from app.models import User, Post
+from app import app, db #db added for Flask Shell
+from app.models import User, Post #Model Instance Added for Flask Shell
 
-@app.shell_context_processor
+@app.shell_context_processor #Decorator register this function as a shell context fn
 def make_shell_context():
     return {'db': db, 'User': User, 'Post' : Post}
