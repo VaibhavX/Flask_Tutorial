d80231304a227da444c9f4ae767bfd4053927ff8
Vaibhav Vishal <vvishal@usc.edu>
2020-10-29 18:54:05 -0700

    Log File Handler and Server Handler for Errors

--- config.py
+++ config.py
@@ -7,3 +7,10 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #Adding Mail Server Configurations to send mail to admin to report an error
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['tunercard1@gmail.com']
